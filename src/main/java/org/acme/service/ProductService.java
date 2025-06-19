800315f5c1cc55358c33a71d578bb68fd8e604c0
jocerdikiawann <cocokcocok33@gmail.com>
2025-06-19 09:34:33 +0700

    add logger in product resource

--- src/main/java/org/acme/service/ProductService.java
+++ src/main/java/org/acme/service/ProductService.java
@@ -7,6 +7,8 @@ import org.acme.model.InsertProductModel;
 import org.acme.model.ResponseModel;
 import org.acme.model.UpdateProductModel;
 import org.acme.repository.ProductRepository;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -14,34 +16,50 @@ import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class ProductService {
+    private static final Logger LOG = LogManager.getLogger(ProductService.class);
 
     @Inject
     ProductRepository productRepo;
 
     public Uni<ResponseModel<Product>> addProduct(InsertProductModel model) {
         return productRepo.createProduct(model.toProduct())
-                .map(data -> new ResponseModel<>("success created", data));
+                .map(data -> {
+                    LOG.info("Product created successfully: " + data.getId());
+                    return new ResponseModel<>("success created", data);
+                });
     }
 
     public Uni<ResponseModel<Product>> updateProduct(Long id, UpdateProductModel model) {
         return productRepo.updateProduct(id, model)
-                .map(data -> new ResponseModel<>("success updated", data));
+                .map(data -> {
+                    LOG.info("Product updated successfully: " + data.getId());
+                    return new ResponseModel<>("success updated", data);
+                });
     }
 
     public Uni<ResponseModel<String>> deleteProduct(Long id) {
         return productRepo.deleteProduct(id)
-                .map(data -> new ResponseModel<>("success deleted", null));
+                .map(data -> {
+                    LOG.info("Product deleted successfully: " + id);
+                    return new ResponseModel<>("success deleted", null);
+                });
     }
 
     public Uni<ResponseModel<Product>> getProduct(Long id) {
         return productRepo.getProductById(id)
-                .map(data -> new ResponseModel<>("success retrieved", data));
+                .map(data -> {
+                    LOG.info("Product retrieved successfully: " + data.getId());
+                    return new ResponseModel<>("success retrieved", data);
+                });
     }
 
     public Uni<ResponseModel<List<Product>>> getAllProducts(String name, Double minPrice, Double maxPrice) {
         return productRepo.getAllProducts(name, minPrice, maxPrice)
                 .collect().asList()
-                .map(data -> new ResponseModel<>("success retrieved", data));
+                .map(data -> {
+                    LOG.info("Products retrieved successfully");
+                    return new ResponseModel<>("success retrieved", data);
+                });
     }
 
 }
